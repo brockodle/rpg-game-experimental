91aafc86afcdda8e81f42543dae5901664f70b40
Odle <bodle@sbma.net>
2022-03-23 16:22:03 -0700

    modified:   buildstats.js 	modified:   index.html 	modified:   templates.js

--- templates.js
+++ templates.js
@@ -1,18 +1,22 @@
-(function(){
-    function player(name, str, dex, wis, int, cha, con, hp, exp, lvl){
+"use strict";
+
+class player {
+    constructor(name, exp, lvl) {
         this.name = name;
-        this.str = str;
-        this.dex = dex;
-        this.wis = wis;
-        this.int = int;
-        this.cha = cha;
-        this.con = con;
-        this.hp = hp;
+        this.str = makestat();
+        this.dex = makestat();
+        this.wis = makestat();
+        this.int = makestat();
+        this.cha = makestat();
+        this.con = makestat();
+        this.hp = 10+con;
         this.exp = exp;
         this.lvl = lvl;
     }
+};
 
-    function monster(name, str, dex, wis, int, cha, con, hp, xpgain){
+class monster {
+    constructor(name, str, dex, wis, int, cha, con, hp, xpgain) {
         this.str = str;
         this.name = name;
         this.dex = dex;
@@ -23,11 +27,20 @@
         this.hp = hp;
         this.xpgain = xpgain;
     }
+};
 
-    function magicobj(buff, stat, worth, xpgain){
+class magicobj {
+    constructor(buff, stat, worth, xpgain) {
         this.buff = buff;
         this.stat = stat;
         this.worth = worth;
         this.xpgain = xpgain;
     }
-}())
+};
+
+function makestat(){
+    console.log('making stats');
+    const statinit = Math.round(Math.random()*20) > 8 === statinit;
+    
+    console.log(statinit);
+}
