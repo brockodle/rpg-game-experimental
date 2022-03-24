e4f5672d7aee9db491834388201ad523efaf03ef
brockodle <48767869+brockodle@users.noreply.github.com>
2022-03-23 23:11:14 -0700

    modified:   index.html 	modified:   templates.js

--- templates.js
+++ templates.js
@@ -1,17 +1,83 @@
 "use strict";
 
+function getmod(tomod) {
+    if (tomod = 30) {
+        return 10;
+    }
+    if (tomod >= 28) {
+        return 9;
+    }
+    if (tomod >= 26) {
+        return 8;
+    }
+    if (tomod >= 24) {
+        return 7
+    }
+    if (tomod >= 22) {
+        return 6;
+    }
+    if (tomod >= 20) {
+        return 5;
+    }
+    if (tomod >= 18) {
+        return 4;
+    }
+    if (tomod >= 16) {
+        return 3;
+    }
+    if (tomod >= 14) {
+        return 2;
+    }
+    if (tomod >= 12) {
+        return 1;
+    }
+    if (tomod >= 10) {
+        return 0;
+    }
+    if (tomod >= 8) {
+        return -1;
+    }
+    if (tomod >= 6) {
+        return -2;
+    }
+    if (tomod >= 4) {
+        return -3;
+    }
+    if (tomod >= 2) {
+        return -4;
+    }
+    if (tomod >= 1) {
+        return -5;
+    }
+};
+
+function commitcharacter() {
+    const currstats = document.getElementById("statrollers");
+}
+
 class player {
     constructor(name, exp, lvl) {
         this.name = name;
-        this.str = makestat();
-        this.dex = makestat();
-        this.wis = makestat();
-        this.int = makestat();
-        this.cha = makestat();
-        this.con = makestat();
-        this.hp = 10+con;
+        this.str = makestat("str");
+        this.strmod = getmod(this.str);
+        this.dex = makestat("dex");
+        this.dexmod = getmod(this.dex);
+        this.wis = makestat("wis");
+        this.wismod = getmod(this.wis);
+        this.int = makestat("int");
+        this.intmod = getmod(this.int);
+        this.cha = makestat("cha");
+        this.chamod = getmod(this.cha);
+        this.con = makestat("con");
+        this.conmod = getmod(this.con);
         this.exp = exp;
         this.lvl = lvl;
+        /*this.class = {
+            addskill1,
+            addskill2,
+            hpmod
+        };
+        this.hp = this.class.hpmod + conmod; */
     }
 };
 
@@ -38,9 +104,24 @@ class magicobj {
     }
 };
 
-function makestat(){
-    console.log('making stats');
-    const statinit = Math.round(Math.random()*20) > 8 === statinit;
-    
-    console.log(statinit);
-}
+function makestat(stat) {
+    console.log('making stats', stat);
+
+    const inblock = document.getElementById(stat);
+
+    console.log(inblock);
+
+    const statcurr = Math.round(Math.random() * 18);
+    const statisovermin = statcurr > 6;
+
+    if (statisovermin) {
+        inblock.value = statcurr;
+        return statcurr;
+    } else {
+        console.log("FALSE D:");
+        makestat(stat);
+    }
+};
+
+let thisplayer = new player("Bobbalobba the Great", 0, 1);
+console.log(thisplayer);
